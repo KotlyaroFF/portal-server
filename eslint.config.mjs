ddf5e08b2efe1e6485a6abf31cbe1081e5b2cacb
Pavel Kotliarov <shpun.0613@gmail.com>
2025-05-18 21:12:58 +0300

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,25 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { createRequire } from 'node:module';
+const baseConfig = createRequire(import.meta.url)('portal-configs/eslint');
+const config = baseConfig.default || baseConfig;
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
+export default [
+  ...config,
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      'no-process-exit': 'error',
+      '@typescript-eslint/no-var-requires': 'error',
+      '@typescript-eslint/require-await': 'warn',
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        {
+          checksVoidReturn: {
+            attributes: false,
+            arguments: true,
+            variables: true,
+            properties: true,
+          },
+        },
+      ],
     },
   },
-);
+];
